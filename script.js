2032f104339650b6a8baae930a60a9f9d90990f7
keyamahtani <41436723+keyamahtani@users.noreply.github.com>
2018-07-19 06:47:57 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function nextSmoothie(){
+    document.getElementById('smoothie').src='smoothieTwo.jpg';
+}
+function lastSmoothie(){
+    document.getElementById('smoothie').src='smoothiebowl.jpg';
+}
+function lastPancakes(){
+    document.getElementById('bananapancakes').src='pancake.jpg';
+}
+function nextPancakes(){
+    document.getElementById('bananapancakes').src='pancakes2.jpg';
+}
+function nextOats(){
+    document.getElementById('oatmeal').src='oats2.jpg';
+}
+function lastOats(){
+    document.getElementById('oatmeal').src='overnight oats.jpg';
+}
